05d8f2bfc3cc179ef15f0eda89fbf6f93cf32be3
imju <imjubyon@gmail.com>
2013-12-27 15:48:32 -0800

    adding KIF but got OBJC_IVAR_$UITouch error

---
+++ AcceptanceTests/AcceptanceTests.m
@@ -0,0 +1,34 @@
+//
+//  AcceptanceTests.m
+//  AcceptanceTests
+//
+//  Created by bgbb on 12/27/13.
+//  Copyright (c) 2013 Edo/Imju. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AcceptanceTests : SenTestCase
+
+@end
+
+@implementation AcceptanceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
